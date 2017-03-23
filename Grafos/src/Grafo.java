4a599d6c584e784c8a1f358f6c118ccd4f9f05f1
profes <profes@PC213>
2017-03-23 19:02:44 -0300

    Implementacion de Grafo.getGrado()

--- Grafos/src/Grafo.java
+++ Grafos/src/Grafo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 
 public class Grafo
 {
@@ -38,10 +40,10 @@ public class Grafo
 	// Verifica que los vértices puedan corresponder a una arista
 	private void chequearArista(int i, int j, String accion)
 	{
-		if( i < 0 || i >= _adj.length )
+		if( i < 0 || i >= getVertices() )
 			throw new IllegalArgumentException("Se intentó " + accion + " una arista con un vértice inexistente! i = " + i);
 		
-		if( j < 0 || j >= _adj.length )
+		if( j < 0 || j >= getVertices() )
 			throw new IllegalArgumentException("Se intentó " + accion + " una arista con un vértice inexistente! j = " + i);
 		
 		if( i == j )
@@ -51,7 +53,7 @@ public class Grafo
 	// El nuevo vértice tiene rótulo n, si antes había n vértices
 	public void agregarVertice()
 	{
-		int n = _adj.length;
+		int n = getVertices();
 		boolean[][] nueva = new boolean[n+1][n+1];
 		
 		for(int i=0; i<n; ++i)
@@ -60,11 +62,46 @@ public class Grafo
 		
 		_adj = nueva;
 	}
+
+	// Retorna el grado (cantidad de vecinos) del vértice i
+	public int getGrado(int i)
+	{
+		// Código defensivo
+		if( i < 0 || i >= getVertices())
+			throw new IllegalArgumentException("Se intentó consultar el grado de un vértice inexistente! i = " + i);
+		
+		int contador = 0;
+		for(int j=0; j<getVertices(); ++j) if( j != i )
+		{
+			if( existeArista(i, j) ) // O(1)
+				++contador;
+		}
+		
+		return contador;
+	}
+	
+	// Cantidad de vértices del grafo
+	public int getVertices()
+	{
+		return _adj.length;
+	}	
 	
 	public static void main(String[] args)
 	{
-		Grafo g = new Grafo(5);
-		g.existeArista(-1, 120); // Esto explota
+		Grafo rueda = new Grafo(6);
+		rueda.agregarArista(0, 1);
+		rueda.agregarArista(1, 2);
+		rueda.agregarArista(2, 3);
+		rueda.agregarArista(3, 4);
+		rueda.agregarArista(4, 0);
+		rueda.agregarArista(0, 5);
+		rueda.agregarArista(1, 5);
+		rueda.agregarArista(2, 5);
+		rueda.agregarArista(3, 5);
+		rueda.agregarArista(4, 5);
+
+		for(int i=0; i<=rueda.getVertices(); ++i)
+			System.out.println("d(" + i + ") = " + rueda.getGrado(i));
 	}
 }
 
